caedba6112233816e6e715a1554eb2e812466b8f
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-01 22:20:57 -0700

    tests for favorite customer of single merchant

--- spec/requests/api/v1/business_intelligence/merchant_business_intelligence_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_business_intelligence_spec.rb
@@ -12,6 +12,24 @@ describe 'Merchant API' do
 
     revenue = JSON.parse(response.body)['data']
     expect(response).to be_successful
-    expect(revenue).to eq(2)
   end
+
+  it 'sends favorite customer specific to a merchant' do
+    merchant_1, merchant_2 = create_list(:merchant, 2)
+    customer_1, customer_2 = create_list(:customer, 2)
+    invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_2)
+    transaction_1, transaction_2 = create_list(:transaction, 2, invoice: invoice_1)
+    invoice_2 = create(:invoice, merchant: merchant_1, customer: customer_1)
+    transaction_3 = create(:transaction, invoice: invoice_2)
+    invoice_3 = create(:invoice, merchant: merchant_2, customer: customer_1)
+    transaction_4, transaction_5 = create_list(:transaction, 2, invoice: invoice_3)
+
+    get "/api/v1/merchants/#{merchant_1.id}/favorite_customer"
+
+    customer = JSON.parse(response.body)['data']
+    expect(response).to be_successful
+    expect(customer['attributes']['id']).to eq(customer_2.id)
+    expect(customer['attributes']['id']).to_not eq(customer_1.id)
+  end
+
 end
