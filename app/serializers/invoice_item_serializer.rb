e13f8ba9f89c844deb5129fee092a8a69703e342
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-01 17:02:29 -0700

    adds unit price conversion of dollars to serializers

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,8 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :quantity, :unit_price, :item_id, :invoice_id
+  attributes :id, :quantity, :item_id, :invoice_id
+
+  attribute :unit_price do |object|
+    object.unit_price / 100.00
+  end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :merchant_id
+
+  attribute :unit_price do |object|
+    object.unit_price / 100.00
+  end
 end
